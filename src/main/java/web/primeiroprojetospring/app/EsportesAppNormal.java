2d7a97ac45c70182bd7f3d639ae832cd0c0695ce
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-14 22:14:29 -0300

    22:14

--- src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -1,14 +1,25 @@
 package web.primeiroprojetospring.app;
 
 import web.primeiroprojetospring.model.Tecnico;
+import web.primeiroprojetospring.model.TecnicoNatacao;
 import web.primeiroprojetospring.model.TecnicoPingPong;
+import web.primeiroprojetospring.service.SorteDiariaFelizService;
+import web.primeiroprojetospring.service.SorteDiariaService;
 
 public class EsportesAppNormal {
 
     public static void main (String[] args) {
 
-        Tecnico tecnico1 = new TecnicoPingPong();
+        SorteDiariaService sorte = new SorteDiariaFelizService();
+
+        Tecnico tecnico1 = new TecnicoPingPong(sorte);
+        TecnicoNatacao tecnico2 = new TecnicoNatacao();
+        tecnico2.setSorteDiariaService(sorte);
+
         System.out.println(tecnico1.getExercicioDiario());
         System.out.println(tecnico1.getSorteDiaria());
+
+        System.out.println(tecnico2.getExercicioDiario());
+        System.out.println(tecnico2.getSorteDiaria());
     }
 }
--- src/main/java/web/primeiroprojetospring/model/TecnicoNatacao.java
+++ src/main/java/web/primeiroprojetospring/model/TecnicoNatacao.java
@@ -3,39 +3,37 @@ package web.primeiroprojetospring.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import web.primeiroprojetospring.service.SorteDiariaService;
+
 public class TecnicoNatacao implements Tecnico {
 
 	private static final Logger logger = LoggerFactory.getLogger(TecnicoNatacao.class);
 	
-//	private SorteDiariaService sorteDiariaService;
+	private SorteDiariaService sorteDiariaService;
 	
 	public TecnicoNatacao() {
 		logger.debug(">> TecnicoNatacao: dentro do construtor padrão");
 	}
 		
-//	public TecnicoNatacao(SorteDiariaService sorteDiariaService) {
-//		logger.debug(">> TecnicoNatacao: dentro do construtor de inicialização");
-//		this.sorteDiariaService = sorteDiariaService;
-//	}
-//
-//	public void setSorteDiariaService(SorteDiariaService sorteDiariaService) {
-//		logger.debug(">> TecnicoNatacao: dentro do setter do SorteDiariaService");
-//		this.sorteDiariaService = sorteDiariaService;
-//	}
+	public TecnicoNatacao(SorteDiariaService sorteDiariaService) {
+		logger.debug(">> TecnicoNatacao: dentro do construtor de inicialização");
+		this.sorteDiariaService = sorteDiariaService;
+	}
+
+	public void setSorteDiariaService(SorteDiariaService sorteDiariaService) {
+		logger.debug(">> TecnicoNatacao: dentro do setter do SorteDiariaService");
+		this.sorteDiariaService = sorteDiariaService;
+	}
 	
 	@Override
 	public String getExercicioDiario() {
 		return "Nade 1000m como aquecimento";
 	}
 
-//	@Override
-//	public String getSorteDiaria() {
-//		return sorteDiariaService.getSorteDiaria();
-//	}
-
 	@Override
 	public String getSorteDiaria() {
-		return "Hoje é seu dia de sorte!";
+		return sorteDiariaService.getSorteDiaria();
 	}
+
 	
 }
