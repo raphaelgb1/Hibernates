067ec38f154a9c96e2469f37ed3cfda23df1bf83
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-14 21:49:53 -0300

    6

--- src/main/java/web/primeiroprojetospring/app/EsportesAppSpring.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppSpring.java
@@ -10,12 +10,15 @@ public class EsportesAppSpring {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EsportesConfig.class);
-    
-        Tecnico tecnico = context.getBean(TecnicoPingPong.class);
-    
-        System.out.println(tecnico.getExercicioDiario());
-        System.out.println(tecnico.getSorteDiaria());
-    
+
+        Tecnico tecnico1 = context.getBean(TecnicoPingPong.class);
+        Tecnico tecnico2 = context.getBean(TecnicoPingPong.class);
+
+        boolean resultado = tecnico1 == tecnico2;
+        System.out.println("Mesmo objeto: " + resultado);
+        System.out.println("tecnico1: " + tecnico1);
+        System.out.println("tecnico2: " + tecnico2);
+
         context.close();
     }
 }
