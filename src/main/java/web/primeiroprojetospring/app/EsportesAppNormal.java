b9c86b232f165913884c199aa87f094c602888d4
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-14 21:32:20 -0300

    Adicionando Técnico de Pingue Pongue

--- src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -1,5 +1,14 @@
 package web.primeiroprojetospring.app;
 
+import web.primeiroprojetospring.model.Tecnico;
+import web.primeiroprojetospring.model.TecnicoPingPong;
+
 public class EsportesAppNormal {
 
+    public static void main (String[] args) {
+
+        Tecnico tecnico1 = new TecnicoPingPong();
+        System.out.println(tecnico1.getExercicioDiario());
+        System.out.println(tecnico1.getSorteDiaria());
+    }
 }
