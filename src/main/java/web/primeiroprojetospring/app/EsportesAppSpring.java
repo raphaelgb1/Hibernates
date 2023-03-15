bb6838fc8dd52e1cf4c77a1d0bbf22fe984b4d99
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-14 21:47:36 -0300

    5.1

--- src/main/java/web/primeiroprojetospring/app/EsportesAppSpring.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppSpring.java
@@ -1,5 +1,21 @@
 package web.primeiroprojetospring.app;
 
-public class EsportesAppSpring {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import web.primeiroprojetospring.config.EsportesConfig;
+import web.primeiroprojetospring.model.Tecnico;
+import web.primeiroprojetospring.model.TecnicoPingPong;
+
+public class EsportesAppSpring {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EsportesConfig.class);
+    
+        Tecnico tecnico = context.getBean(TecnicoPingPong.class);
+    
+        System.out.println(tecnico.getExercicioDiario());
+        System.out.println(tecnico.getSorteDiaria());
+    
+        context.close();
+    }
 }
