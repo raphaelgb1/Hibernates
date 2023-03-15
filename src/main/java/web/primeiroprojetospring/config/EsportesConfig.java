c7bdca06e962963049bbbbe7101e8fe76c917fc4
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-14 21:57:44 -0300

    14

--- src/main/java/web/primeiroprojetospring/config/EsportesConfig.java
+++ src/main/java/web/primeiroprojetospring/config/EsportesConfig.java
@@ -1,18 +1,26 @@
 package web.primeiroprojetospring.config;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import web.primeiroprojetospring.model.Tecnico;
+import web.primeiroprojetospring.model.TecnicoNatacao;
 
 @Configuration
 @ComponentScan("web.primeiroprojetospring.model")
 public class EsportesConfig {
     private static final Logger logger = LoggerFactory.getLogger(EsportesConfig.class);
-	
-	public EsportesConfig() {
-		logger.debug(">> EsportesConfig: dentro do construtor padrão");
-	}
+
+    public EsportesConfig() {
+        logger.debug(">> EsportesConfig: dentro do construtor padrão");
+    }
+
+    @Bean
+    public Tecnico tecnicoNatacao() {
+        logger.debug(">> EsportesConfig: dentro do método fábrica tecnicoNatacao");
+        return new TecnicoNatacao();
+    }
 }
