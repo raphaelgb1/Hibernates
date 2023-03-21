1f9cb0bd4bd78c9730b91cc7a31d73ff008c641a
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-21 19:42:22 -0300

    asd

--- src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -4,12 +4,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import web.primeiroprojetospring.config.EsportesConfig;
 import web.primeiroprojetospring.model.Tecnico;
+import web.primeiroprojetospring.service.SorteDiariaArquivoService;
+import web.primeiroprojetospring.service.SorteDiariaFelizService;
+import web.primeiroprojetospring.service.SorteDiariaRandomicaService;
+import web.primeiroprojetospring.service.SorteDiariaService;
+import web.primeiroprojetospring.service.SorteDiariaTristeService;
 
 public class EsportesAppNormal {
 
     public static void main (String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EsportesConfig.class);
+        
+        // TecnicoBaseball 	→ SorteDiariaTristeService
+        // TecnicoPingPong 	→ SorteDiariaFelizService
+        // TecnicoTenis 	→ SorteDiariaArquivoService (precisa carregar as sortes do arquivo!)
+        // TecnicoNatacao 	→ SorteDiariaRandomicaService
+        
+        SorteDiariaService feliz = new SorteDiariaFelizService();
+        SorteDiariaService triste = new SorteDiariaTristeService();
+        SorteDiariaService rand = new SorteDiariaRandomicaService();
+        SorteDiariaService arquivo = new SorteDiariaArquivoService();
 
         Tecnico tecnicos[] = new Tecnico[4];
 
