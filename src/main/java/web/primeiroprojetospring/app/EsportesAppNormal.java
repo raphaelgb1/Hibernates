34ad71ceac72322864f732e78cc66c22fdf012c5
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-21 19:30:23 -0300

    tecnicos

--- src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -3,6 +3,7 @@ package web.primeiroprojetospring.app;
 import web.primeiroprojetospring.model.Tecnico;
 import web.primeiroprojetospring.model.TecnicoNatacao;
 import web.primeiroprojetospring.model.TecnicoPingPong;
+import web.primeiroprojetospring.model.TecnicoTenis;
 import web.primeiroprojetospring.service.SorteDiariaFelizService;
 import web.primeiroprojetospring.service.SorteDiariaService;
 
@@ -11,15 +12,18 @@ public class EsportesAppNormal {
     public static void main (String[] args) {
 
         SorteDiariaService sorte = new SorteDiariaFelizService();
-
+        Tecnico tecnicos[] = new Tecnico[4];
+        tecnicos[0] = new TecnicoPingPong(sorte);
         Tecnico tecnico1 = new TecnicoPingPong(sorte);
+        tecnicos[1] = tecnico1;
+        tecnicos[2] = new TecnicoTenis(sorte);
         TecnicoNatacao tecnico2 = new TecnicoNatacao();
         tecnico2.setSorteDiariaService(sorte);
+        tecnicos[3] = tecnico2;
 
-        System.out.println(tecnico1.getExercicioDiario());
-        System.out.println(tecnico1.getSorteDiaria());
-
-        System.out.println(tecnico2.getExercicioDiario());
-        System.out.println(tecnico2.getSorteDiaria());
+        for (Tecnico tecnico: tecnicos) {
+            System.out.println(tecnico1.getExercicioDiario());
+            System.out.println(tecnico1.getSorteDiaria());
+        }
     }
 }
