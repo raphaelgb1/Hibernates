605d343c29510c5dc4ee48c43a0ee1674c9a28c6
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-21 19:38:56 -0300

    asd

--- src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
+++ src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -1,29 +1,28 @@
 package web.primeiroprojetospring.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import web.primeiroprojetospring.config.EsportesConfig;
 import web.primeiroprojetospring.model.Tecnico;
-import web.primeiroprojetospring.model.TecnicoNatacao;
-import web.primeiroprojetospring.model.TecnicoPingPong;
-import web.primeiroprojetospring.model.TecnicoTenis;
-import web.primeiroprojetospring.service.SorteDiariaFelizService;
-import web.primeiroprojetospring.service.SorteDiariaService;
 
 public class EsportesAppNormal {
 
     public static void main (String[] args) {
 
-        SorteDiariaService sorte = new SorteDiariaFelizService();
-        Tecnico tecnicos[] = new Tecnico[4];
-        tecnicos[0] = new TecnicoPingPong(sorte);
-        Tecnico tecnico1 = new TecnicoPingPong(sorte);
-        tecnicos[1] = tecnico1;
-        tecnicos[2] = new TecnicoTenis(sorte);
-        TecnicoNatacao tecnico2 = new TecnicoNatacao();
-        tecnico2.setSorteDiariaService(sorte);
-        tecnicos[3] = tecnico2;
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EsportesConfig.class);
 
-        for (Tecnico tecnico: tecnicos) {
-            System.out.println(tecnico.getExercicioDiario());
-            System.out.println(tecnico.getSorteDiaria());
+        Tecnico tecnicos[] = new Tecnico[4];
+
+        tecnicos[0] = context.getBean("xurumela", Tecnico.class);
+        tecnicos[1] = context.getBean("tecnicoNatacao", Tecnico.class);
+        tecnicos[2] = context.getBean("tecnicoTenis", Tecnico.class);
+        tecnicos[3] = context.getBean("tecnicoBaseball", Tecnico.class);
+
+        for (Tecnico t : tecnicos) {
+            System.out.println(t.getExercicioDiario());
+            System.out.println(t.getSorteDiaria());
         }
+
+        context.close();
     }
 }
--- src/main/java/web/primeiroprojetospring/model/TecnicoPingPong.java
+++ src/main/java/web/primeiroprojetospring/model/TecnicoPingPong.java
@@ -2,7 +2,6 @@ package web.primeiroprojetospring.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import web.primeiroprojetospring.service.SorteDiariaService;
